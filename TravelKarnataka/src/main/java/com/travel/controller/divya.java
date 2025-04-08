6d1f18b201af131303e82dd13dcf9376cb770d8b
Divyashree <divya01@gmail.com>
2025-04-08 22:13:46 +0530

    Created Review  html and that entity,repo,service,serimpl

--- TravelKarnataka/src/main/java/com/travel/controller/divya.java
+++ TravelKarnataka/src/main/java/com/travel/controller/divya.java
@@ -1,5 +1,38 @@
 package com.travel.controller;
 
-public class divya {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.travel.entity.ReviewEntity;
+import com.travel.service.ReviewService;
+import jakarta.servlet.http.HttpSession;
+
+public class divya {
+	
+	@Autowired
+	private ReviewService reviewservice;
+	
+	
+//	if ((repo.findByUserEmail(email).getUserEmail().equals(email))
+//			&& (repo.findByUserEmail(email).getUserPassword().equals(password))) {
+//		System.out.println("login Succesfull...........");
+//		session.setAttribute("umail", repo.findByUserEmail(email).getUserEmail());
+//		session.setAttribute("uname", repo.findByUserEmail(email).getUserName());
+//		session.setAttribute("uphone", repo.findByUserEmail(email).getUserPhone());
+//	}
+	@PostMapping("/review")
+	public String userReview(@ModelAttribute ReviewEntity review, Model model,HttpSession session) {
+		model.addAttribute("uname", session.getAttribute("uname"));
+		model.addAttribute("umail", session.getAttribute("umail"));
+		int rid=reviewservice.saveReview(review);
+		if(rid>0) {
+			model.addAttribute("msg","Thank you for your Feedback");
+			return "review";
+		}else {
+			model.addAttribute("msg","Failed to recieve your Feedback");
+			return "review";
+		}
+	}
 }
---
+++ TravelKarnataka/src/main/java/com/travel/service/ReviewService.java
@@ -0,0 +1,9 @@
+package com.travel.service;
+
+import com.travel.entity.ReviewEntity;
+
+public interface ReviewService {
+
+	int saveReview(ReviewEntity review);
+
+}
